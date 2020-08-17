ec077bfa9e93a023f84c8afd23d33069e209a7d0
Connor White <connor.bcw@gmail.com>
2020-08-16 23:53:39 -0500

    add support for nested cwd

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,17 @@
-import { join } from "path";
 import { PackageJson } from "type-fest";
+import findUp from "find-up";
 import { readJSONFileSync } from "read-json-safe";
 import version, { getVersion } from "./version";
 
 export type PackageJSON = PackageJson;
 
 export function getPackageJSON(dirname: string): PackageJSON | undefined {
-  return readJSONFileSync(join(dirname, "package.json"));
+  const path = findUp.sync("package.json", {
+    cwd: dirname
+  });
+  if(path) {
+    return readJSONFileSync(path);
+  }
 }
 
 export {
