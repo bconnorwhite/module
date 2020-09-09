4278c77335e515de906d99b9a750d32fc8c45af4
Connor White <connor.bcw@gmail.com>
2020-09-08 20:51:55 -0500

    update to bob 2

--- source/package-json/version.ts
+++ source/package-json/version.ts
@@ -1,11 +1,11 @@
 import { getPackageJSON, getPackageJSONSync } from ".";
 
-export async function getVersion(dirname: string, relative?: string) {
+export async function getVersion(dirname: string, relative?: string): Promise<string | undefined> {
   return getPackageJSON(dirname, relative).then((pkg) => {
     return pkg?.version;
   });
 }
 
-export function getVersionSync(dirname: string, relative?: string) {
+export function getVersionSync(dirname: string, relative?: string): string | undefined {
   return getPackageJSONSync(dirname, relative)?.version;
 }
